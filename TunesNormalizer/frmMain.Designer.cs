6b002859092063423ffc03d99936ac061942f913
Christian <Christian@DESKTOP-74V296K>
2021-10-16 19:33:46 -0400

    Add project files.

---
+++ TunesNormalizer/frmMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace TunesNormalizer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelMain = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelTunesDirectory = new System.Windows.Forms.Panel();
+            this.buttonSelectDirectory = new System.Windows.Forms.Button();
+            this.textTunesDirectory = new System.Windows.Forms.TextBox();
+            this.labelTunesDirectory = new System.Windows.Forms.Label();
+            this.buttonBeginNormalization = new System.Windows.Forms.Button();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panelMain.SuspendLayout();
+            this.panelTunesDirectory.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMain
+            // 
+            this.panelMain.AutoSize = true;
+            this.panelMain.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelMain.Controls.Add(this.panelTunesDirectory);
+            this.panelMain.Controls.Add(this.buttonBeginNormalization);
+            this.panelMain.Controls.Add(this.labelProgress);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.panelMain.Location = new System.Drawing.Point(0, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(569, 88);
+            this.panelMain.TabIndex = 0;
+            // 
+            // panelTunesDirectory
+            // 
+            this.panelTunesDirectory.AutoSize = true;
+            this.panelTunesDirectory.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelTunesDirectory.Controls.Add(this.buttonSelectDirectory);
+            this.panelTunesDirectory.Controls.Add(this.textTunesDirectory);
+            this.panelTunesDirectory.Controls.Add(this.labelTunesDirectory);
+            this.panelTunesDirectory.Location = new System.Drawing.Point(3, 3);
+            this.panelTunesDirectory.Name = "panelTunesDirectory";
+            this.panelTunesDirectory.Size = new System.Drawing.Size(558, 29);
+            this.panelTunesDirectory.TabIndex = 0;
+            // 
+            // buttonSelectDirectory
+            // 
+            this.buttonSelectDirectory.Location = new System.Drawing.Point(434, 3);
+            this.buttonSelectDirectory.Name = "buttonSelectDirectory";
+            this.buttonSelectDirectory.Size = new System.Drawing.Size(121, 23);
+            this.buttonSelectDirectory.TabIndex = 2;
+            this.buttonSelectDirectory.Text = "Select Directory";
+            this.buttonSelectDirectory.UseVisualStyleBackColor = true;
+            this.buttonSelectDirectory.Click += new System.EventHandler(this.buttonSelectDirectory_Click);
+            // 
+            // textTunesDirectory
+            // 
+            this.textTunesDirectory.Location = new System.Drawing.Point(97, 5);
+            this.textTunesDirectory.Name = "textTunesDirectory";
+            this.textTunesDirectory.Size = new System.Drawing.Size(331, 20);
+            this.textTunesDirectory.TabIndex = 1;
+            this.textTunesDirectory.TextChanged += new System.EventHandler(this.textTunesDirectory_TextChanged);
+            // 
+            // labelTunesDirectory
+            // 
+            this.labelTunesDirectory.AutoSize = true;
+            this.labelTunesDirectory.Location = new System.Drawing.Point(6, 8);
+            this.labelTunesDirectory.Name = "labelTunesDirectory";
+            this.labelTunesDirectory.Size = new System.Drawing.Size(85, 13);
+            this.labelTunesDirectory.TabIndex = 0;
+            this.labelTunesDirectory.Text = "Tunes Directory:";
+            // 
+            // buttonBeginNormalization
+            // 
+            this.buttonBeginNormalization.Enabled = false;
+            this.buttonBeginNormalization.Location = new System.Drawing.Point(3, 38);
+            this.buttonBeginNormalization.Name = "buttonBeginNormalization";
+            this.buttonBeginNormalization.Size = new System.Drawing.Size(121, 23);
+            this.buttonBeginNormalization.TabIndex = 3;
+            this.buttonBeginNormalization.Text = "Begin Normalization";
+            this.buttonBeginNormalization.UseVisualStyleBackColor = true;
+            this.buttonBeginNormalization.Click += new System.EventHandler(this.buttonBeginNormalization_Click);
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(3, 64);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(48, 13);
+            this.labelProgress.TabIndex = 0;
+            this.labelProgress.Text = "Progress";
+            this.labelProgress.Visible = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(569, 88);
+            this.Controls.Add(this.panelMain);
+            this.Name = "frmMain";
+            this.Text = "Tunes Normalizer";
+            this.panelMain.ResumeLayout(false);
+            this.panelMain.PerformLayout();
+            this.panelTunesDirectory.ResumeLayout(false);
+            this.panelTunesDirectory.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel panelMain;
+        private System.Windows.Forms.Panel panelTunesDirectory;
+        private System.Windows.Forms.Button buttonSelectDirectory;
+        private System.Windows.Forms.TextBox textTunesDirectory;
+        private System.Windows.Forms.Label labelTunesDirectory;
+        private System.Windows.Forms.Button buttonBeginNormalization;
+        private System.Windows.Forms.Label labelProgress;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ TunesNormalizer/frmMain.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TunesNormalizer
+{
+    public partial class frmMain : Form
+    {
+        #region Constants
+
+        private const int COUNT_SENTINEL = -1;
+        private readonly char[] INVALID_FILE_CHARS = Path.GetInvalidFileNameChars();
+        private readonly char[] INVALID_PATH_CHARS = Path.GetInvalidPathChars();
+
+        #endregion
+
+        #region Data Members
+
+        private int _iTotalSongs, _iNormalizedSongs;
+        private Thread _threadNormalizer;
+
+        #endregion
+
+        #region Construction
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            _iTotalSongs = COUNT_SENTINEL;
+            _threadNormalizer = new Thread(new ThreadStart(OnThreadNormalize));
+        }
+
+        #endregion
+
+        #region Event Handling
+
+        private void buttonSelectDirectory_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                var result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    this.textTunesDirectory.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void buttonBeginNormalization_Click(object sender, EventArgs e)
+        {
+            if (_threadNormalizer != null && _threadNormalizer.IsAlive)
+                _threadNormalizer.Abort();
+
+            _threadNormalizer = new Thread(new ThreadStart(OnThreadNormalize));
+            _threadNormalizer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (_iTotalSongs == COUNT_SENTINEL)
+                this.labelProgress.Visible = false;
+            else
+            {
+                this.labelProgress.Visible = true;
+                this.labelProgress.Text = String.Format("{0} / {1} Songs Normalized", _iNormalizedSongs, _iTotalSongs);
+            }
+        }
+
+        private void textTunesDirectory_TextChanged(object sender, EventArgs e)
+        {
+            this.buttonBeginNormalization.Enabled = Directory.Exists(this.textTunesDirectory.Text);
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        protected virtual void OnThreadNormalize ()
+        {
+            string sDirectory = this.textTunesDirectory.Text;
+            string sNormalizedDirectory = sDirectory + "\\NormalizedTunes";
+
+            // Begin by making a "Normalized" folder within the selected directory.
+            GenerateDirectory(sNormalizedDirectory);
+
+            // Next, retrieve the number of MP3 files to normalize.
+            string[] mp3Files = Directory.GetFiles(sDirectory, "*.mp3");
+            _iTotalSongs = mp3Files.Length;
+            _iNormalizedSongs = 0;
+
+            // Finally, iterate through the list and apply normalization.
+            foreach (var sOriginalFile in mp3Files)
+            {
+                var tagFile = TagLib.File.Create(sOriginalFile);
+                uint iTrack = tagFile.Tag.Track;
+                string sTitle = SanitizeFile (String.Format("[{0}] - {1}", iTrack.ToString ("00"), tagFile.Tag.Title)).Trim ();
+                string sArtist = tagFile.Tag.AlbumArtists.Length > 0 ? tagFile.Tag.AlbumArtists [0].Trim () : "Unknown Artist";
+                string sAlbum = string.IsNullOrEmpty(tagFile.Tag.Album) ? "Unknown Album" : tagFile.Tag.Album.Trim();
+                string sNormalizedPath = SanitizePath(sNormalizedDirectory + "\\" + sArtist + "\\" + sAlbum);
+
+                sTitle = GenerateDirectory(sNormalizedPath) ?
+                    sNormalizedPath + "\\" + sTitle :
+                    sNormalizedDirectory + "\\" + Path.GetFileName(sOriginalFile);
+
+                if (!sTitle.EndsWith(".mp3"))
+                    sTitle += ".mp3";
+
+                if (!File.Exists (sTitle))
+                    File.Copy(sOriginalFile, sTitle);
+                _iNormalizedSongs++;
+            }
+        }
+
+        protected virtual bool GenerateDirectory (string sDirectory)
+        {
+            try
+            {
+                if (!Directory.Exists(sDirectory))
+                    Directory.CreateDirectory(sDirectory);
+            }
+            catch (Exception)
+            {
+               return false;
+            }
+
+            return true;
+        }
+
+        protected virtual string SanitizeFile (string sTitle)
+        {
+            foreach (var invalidChar in INVALID_FILE_CHARS)
+                sTitle = sTitle.Replace(invalidChar, '_');
+
+            return sTitle;
+        }
+
+        protected virtual string SanitizePath (string sPath)
+        {
+            foreach (var invalidChar in INVALID_PATH_CHARS)
+                sPath = sPath.Replace(invalidChar, '_');
+
+            return sPath;
+        }
+
+        #endregion
+    }
+}
